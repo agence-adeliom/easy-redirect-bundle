3e5c16489bc45240a71a8db2fed8288dc1816884
Arnaud RITTI <arnaud.ritti@adeliom.com>
2021-12-20 16:31:37 +0100

    no message

--- src/EasyRedirectBundle.php
+++ src/EasyRedirectBundle.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EasyRedirectBundle extends Bundle
 {
+    /**
+     * @return ExtensionInterface|null The container extension
+     */
     public function getContainerExtension()
     {
         return new EasyRedirectExtension();
